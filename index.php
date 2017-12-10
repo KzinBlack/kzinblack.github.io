ddfdcfafb6b8d97790d5eb07af63074b12c150e8
KzinBlack <islam.bayrahtar@gmail.com>
2017-12-10 15:34:12 +0300

    Здесь пытаемся создать шапку и адаптировать его !
    Signed-off-by: KzinBlack <islam.bayrahtar@gmail.com>

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>itProger - Сайт для разработчиков</title>
+	<link rel="stylesheet" href="css/main.css" type="text/css" charset="utf-8">
+	<meta name="description" content="Информация-развлекательный портал для программистов ! Обучающие курсы, новости в сфере IT и многое другое">
+	<meta name="keywords" content="itProger, it, курсы, новости, уроки">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href=:"css/main.css" type="image/x-icon">
+	</head>
+	<body>
+<header>
+<div id="logo">
+	<a href="http://itproger.havi/" title="На главную">
+		<img src="img/logo.png" title="itProger" alt="itProger.com">
+		<span>itProger</span>
+	</a>
+</div>	
+<div id="about">
+	<a href="" title="Узнать деталнее об рекламе">Реклама</a>
+	<a href="" title="Написать нам писмо">Обратная связь</a>
+</div>
+<div id="reg_auth">
+	<a href="" title="Войти в кабинет пользователя">
+	<div class="btn">
+		Войти
+		</a>
+		<a href="" title="Зарегистрироваться  на сайте">
+			<div class="btn">
+				Регистрация
+			</div>
+		</a>
+	</div>
+</div>
+	</header>
+</body>
+</html>
